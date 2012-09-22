db31265e7701ec1974f260c45d38d9773b935408
Max Penet <m@qbits.cc>
2012-09-22 11:07:28 +0200

    add mimetype-converter support to ajax request

--- src/jayq/core.cljs
+++ src/jayq/core.cljs
@@ -1,6 +1,7 @@
 (ns jayq.core
   (:refer-clojure :exclude [val empty remove find])
-  (:require [clojure.string :as string])
+  (:require [clojure.string :as string]
+            [cljs.reader :as reader])
   (:use [jayq.util :only [clj->js]]))
 
 (defn crate-meta [func]
@@ -194,6 +195,19 @@
     (.ajax js/jQuery url (clj->js settings)))
   ([settings]
     (.ajax js/jQuery (clj->js settings))))
+
+(defn ^:private mimetype-converter [s]
+  (reader/read-string (str s)))
+
+(.ajaxSetup js/jQuery
+ (clj->js
+  {:accepts {:edn "application/edn, text/edn"
+             :clj "application/clj, text/clj"}
+   :contents {"clj" #"edn|clj"}
+   :converters
+   {"text edn" mimetype-converter
+    "text clj" mimetype-converter}}))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Events
